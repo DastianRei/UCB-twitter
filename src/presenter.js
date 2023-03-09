0817f6a72d86ea2765f5256664952d1e311373b9
Dayan <dayan.aguilar@ucb.edu.bo>
2023-03-09 09:06:52 -0400

    Creamos la funcion validar formulario

--- src/presenter.js
+++ src/presenter.js
@@ -2,6 +2,25 @@ const titulo = document.querySelector("#titulo");
 const detalle = document.querySelector("#detalle");
 const formulario = document.querySelector("#formulario");
 
-formulario.addEventListener("submit",(event)=>{
-  
-})
+const Post = {
+  titulo: "",
+  detalle: "",
+  fecha: "",
+};
+
+let Publicaciones = [];
+
+formulario.addEventListener("submit", validarFormulario);
+
+function validarFormulario(e) {
+  e.preventDefault();
+  if (titulo.value === "") {
+    alert("No es posible publicar un post sin título");
+    return;
+  } else if (titulo.value <= 50 && detalle.value <= 150) {
+    Post.titulo = titulo.value;
+    Post.detalle = detalle.value;
+    Post.fecha = new Date();
+    console.log(Post);
+  }
+}
