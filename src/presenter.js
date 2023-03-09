ce7c1343c215b3a40f375ba24dd9f29f38442d76
Dayan <dayan.aguilar@ucb.edu.bo>
2023-03-09 09:00:46 -0400

    Agarramos las variables del html

---
+++ src/presenter.js
@@ -0,0 +1,7 @@
+const titulo = document.querySelector("#titulo");
+const detalle = document.querySelector("#detalle");
+const formulario = document.querySelector("#formulario");
+
+formulario.addEventListener("submit",(event)=>{
+  
+})
